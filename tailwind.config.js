eb1096f7f4a4f9972c096b19815e53d25bed09b6
Kelly <50586093+kelly-jsx@users.noreply.github.com>
2023-02-28 15:29:24 +0100

    initial commit, preconfiguration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,26 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        blue: {
+          marine: 'hsl(213, 96%, 18%)',
+          purplish: 'hsl(243, 100%, 62%)',
+          pastel: 'hsl(228, 100%, 84%)',
+          light: 'hsl(206, 94%, 87%)'
+        },
+        red: {
+          strawberry: 'hsl(354, 84%, 57%)'
+        },
+        gray: {
+          cool: 'hsl(231, 11%, 63%)',
+          light: 'hsl(229, 24%, 87%)'
+        },
+        mangolia: 'hsl(217, 100%, 97%)',
+        alabaster: 'hsl(231, 100%, 99%)',
+        white: 'hsl(0, 0%, 100%)'
+      }
+    }
   },
   plugins: []
 }

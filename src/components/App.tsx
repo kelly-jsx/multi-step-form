7eb50f1c3bbf8b046c9c0c36ff3d4bbada5cddf9
Kelly <50586093+kelly-jsx@users.noreply.github.com>
2023-02-28 17:04:31 +0100

    Bottom navigation component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import sidebarMobile from './../public/bg-sidebar-mobile.svg'
+import { BottomNav } from './BottomNav'
 
 import { Form } from './Form/Form'
 import { Header } from './Header/Header'
@@ -7,7 +8,7 @@ function App() {
   return (
     <div className="h-screen w-screen bg-mangolia">
       <div
-        className="flex h-screen w-screen flex-col items-center gap-8 px-4 pt-8"
+        className="flex h-max w-screen flex-col items-center gap-8 px-4 pt-8"
         style={{
           backgroundImage: `url(${sidebarMobile})`,
           backgroundRepeat: 'no-repeat'
@@ -15,6 +16,7 @@ function App() {
       >
         <Header />
         <Form />
+        <BottomNav />
       </div>
     </div>
   )
---
+++ src/components/BottomNav.tsx
@@ -0,0 +1,9 @@
+export const BottomNav = () => {
+  return (
+    <section className="fixed inset-x-0 bottom-0 z-10 flex justify-end bg-white p-4 shadow">
+      <button className="rounded-md bg-blue-marine p-2 text-white">
+        Next Step
+      </button>
+    </section>
+  )
+}

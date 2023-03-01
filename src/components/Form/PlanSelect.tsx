e48f157e588e86115e46f16d9e02e4b004a65fba
Kelly <50586093+kelly-jsx@users.noreply.github.com>
2023-03-01 12:51:13 +0100

    PlanSelect is input radio now

--- src/components/Form/PlanSelect.tsx
+++ src/components/Form/PlanSelect.tsx
@@ -6,14 +6,20 @@ type Props = {
 }
 
 export const PlanSelect = ({ imgUrl, title, price, yearly }: Props) => (
-  <div className="flex w-full gap-4 rounded-lg border px-4 py-2">
-    <img src={imgUrl} alt={title} />
-    <div className="flex flex-col">
-      <h1 className="text-base font-bold capitalize text-blue-marine">
-        {title}
-      </h1>
-      <p className="text-sm text-gray-cool">{price}</p>
-      {yearly && <p className="text-xs">2 months free</p>}
-    </div>
-  </div>
+  <>
+    <input type="radio" name="plan" id={title} className="plan-radio hidden" />
+    <label
+      className="plan-card flex w-full gap-4 rounded-lg border px-4 py-2"
+      htmlFor={title}
+    >
+      <img src={imgUrl} alt={title} />
+      <div className="flex flex-col">
+        <h1 className="text-base font-bold capitalize text-blue-marine">
+          {title}
+        </h1>
+        <p className="text-sm text-gray-cool">{price}</p>
+        {yearly && <p className="text-xs">2 months free</p>}
+      </div>
+    </label>
+  </>
 )
